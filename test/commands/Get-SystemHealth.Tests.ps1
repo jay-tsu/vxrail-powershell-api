8ec2d9def7ed4c7d60720c789f582068b37fd290
Jay Xu <jay.xu@emc.com>
2018-02-24 16:06:24 +0800

    Fix Get-SystemHealth test issue.

--- test/commands/Get-SystemHealth.Tests.ps1
+++ test/commands/Get-SystemHealth.Tests.ps1
@@ -2,7 +2,7 @@ Describe  'Get-SystemHealth checks' {
 
     Context 'Checking the function Get-SystemHealth.' {
         It 'The function Get-SystemHealth() is running' {
-            Get-SystemHealth -server 10.62.81.84 -username user -password password1 | Should be "OK"
+            Get-SystemHealth -server 10.62.81.84 -username user -password password | Should be "OK"
         }
 
     }
